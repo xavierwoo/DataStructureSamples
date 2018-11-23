121dc47635c8d22b83c7cb825ca5267ba83d7407
xavierwoo <xavier.woo@gmail.com>
2018-11-23 10:47:11 +0800

    Initial Commit

---
+++ DataStructureSamples/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructureSamples
+//
+//  Created by 吴 歆韵 on 2018/11/23.
+//  Copyright © 2018 Xavier Woo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
